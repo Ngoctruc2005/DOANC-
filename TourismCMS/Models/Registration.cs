b66748241d1f5e93bfebca2b5c3eb78488591ebb
Thanh Tuyền <ltthahtyn945@gmail.com>
2026-03-27 23:01:48 +0700

    owner

---
+++ TourismCMS/Models/Registration.cs
@@ -0,0 +1,13 @@
+﻿namespace TourismCMS.Models
+{
+    public class Registration
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int POIId { get; set; }
+
+        public string Status { get; set; }
+    }
+}
